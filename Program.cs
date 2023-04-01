fa630cc57b536459ded785004a2da4d690ba9a1e
Letnan Degurchaff <LetnanDegurchaffx@gmail.com>
2023-04-01 15:51:05 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeaponsReport
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MilitaryUnit militaryUnit = new MilitaryUnit();
+            militaryUnit.Work();
+        }
+    }
+
+    class MilitaryUnit
+    {
+        private List<Soldier> _soldiers;
+
+        public MilitaryUnit()
+        {
+            SoldierCreator soldierCreator = new SoldierCreator();
+            int soldiersCount = 10;
+
+            _soldiers = soldierCreator.CreateSoldiers(soldiersCount);
+        }
+
+        public void Work()
+        {
+            ConsoleOutputMethods.WriteRedText("Полная информация о солдатах");
+            ShowFullSoldierInfo();
+
+            ConsoleOutputMethods.WriteRedText("Краткая информация о солдатах");
+            ShowNamesAndRanks();
+        }
+
+        private void ShowFullSoldierInfo()
+        {
+            foreach (var soldierInfo in _soldiers)
+            {
+                Console.WriteLine($"{soldierInfo.FullName}\n" +
+                    $"{soldierInfo.Weapon}\n" +
+                    $"{soldierInfo.Rank}\n" +
+                    $"Месяцев на службе - {soldierInfo.ServiceTerm}\n");
+            }
+        }
+
+        private void ShowNamesAndRanks()
+        {
+            var soldiersList = _soldiers
+                .Select(soldier => soldier.FullName + "\n" + soldier.Rank)
+                .ToList();
+
+            foreach (var soldierInfo in soldiersList)
+            {
+                Console.WriteLine(soldierInfo + "\n");
+            }
+        }
+    }
+
+    class Soldier
+    {
+        public Soldier(string fullName, string weapon, string rank, int serviceTerm)
+        {
+            FullName = fullName;
+            Weapon = weapon;
+            Rank = rank;
+            ServiceTerm = serviceTerm;
+        }
+
+        public string FullName { get; private set; }
+        public string Weapon { get; private set; }
+        public string Rank { get; private set; }
+        public int ServiceTerm { get; private set; }
+    }
+
+    class SoldierCreator
+    {
+        private List<string> _names;
+        private List<string> _weapons;
+        private List<string> _ranks;
+
+        public SoldierCreator()
+        {
+            _names = new List<string>
+            {
+                "Пупкин Василий Викторович",
+                "Сафронов Алексей Николаевич",
+                "Табуретов Биба Васильевич",
+                "Табуретов Боба Васильевич",
+                "Котофеев Нурлан Барсикович",
+                "Рыбаков Александр Юрьевич",
+                "Владимиров Владимир Владимирович"
+            };
+
+            _weapons = new List<string>
+            {
+                "AK-74M",
+                "AK-12",
+                "AK-47U"
+            };
+
+            _ranks = new List<string>
+            {
+                "Recruit",
+                "Corporal"
+            };
+        }
+
+        public List<Soldier> CreateSoldiers(int count)
+        {
+            Random random = new Random();
+            List<Soldier> soldiers = new List<Soldier>();
+
+            int maxServiceTerm = 12;
+
+            for (int i = 0; i < count; i++)
+            {
+                soldiers.Add(new Soldier(_names[random.Next(0,_names.Count)],
+                    _weapons[random.Next(0, _weapons.Count)],
+                    _ranks[random.Next(0, _ranks.Count)],
+                    random.Next(0, maxServiceTerm)));
+            }
+
+            return soldiers;
+        }
+    }
+
+    static class ConsoleOutputMethods
+    {
+        public static void WriteRedText(string text)
+        {
+            ConsoleColor tempColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(text);
+            Console.ForegroundColor = tempColor;
+        }
+    }
+}
